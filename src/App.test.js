f56d3514067c37fb7220bbf073cd36774c4e79e1
li-lauren <laurenlidev@gmail.com>
2021-04-01 14:01:03 -0600

    Test 'how it works' link

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,32 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
+import { MemoryRouter } from 'react-router';
+import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+// define header test suite
+describe('Header', () => {
+  test('"How it works" link points to the correct page', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+    // print out the dom
+    // screen.debug();
+
+    const link = screen.getByRole('link', { name: /how it works/i });
+    // screen.debug(link);
+    userEvent.click(link);
+    
+    expect(
+      screen.getByRole('heading', { name: /how it works/i })
+    ).toBeInTheDocument();
+  });
 });
